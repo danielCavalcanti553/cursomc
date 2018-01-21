1c6b98b85d041216780f0a487da951fee0450238
Daniel Cavalcanti <daniel.cavalcanti@outlook.com.br>
2018-01-21 17:27:13 -0200

    Produto Categoria associação muitos para muitos

--- src/main/java/com/daniel/cursomc/domain/Categoria.java
+++ src/main/java/com/daniel/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.daniel.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -16,6 +19,9 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {}
 
@@ -41,6 +47,14 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -65,6 +79,7 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
-	
+
+
 	
 }
