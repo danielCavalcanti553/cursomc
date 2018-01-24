db47cf08549dd973d1a7e6343375c212d1beadf9
Daniel Cavalcanti <daniel.cavalcanti@outlook.com.br>
2018-01-24 02:20:24 -0200

    Modelagem Pedido, Pagamento

--- src/main/java/com/daniel/cursomc/domain/Cliente.java
+++ src/main/java/com/daniel/cursomc/domain/Cliente.java
@@ -41,6 +41,10 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	// associado a pedido
+	private List<Pedido> pedidos = new ArrayList<Pedido>();
+	
+	
 	public Cliente() {	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
@@ -107,6 +111,14 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -132,6 +144,8 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
+
+
 	
 	
 	
