9683eb3845c4763d420c627effce9181cbaab3a9
Daniel Cavalcanti <daniel.cavalcanti@outlook.com.br>
2018-02-03 17:16:39 -0200

    Validate Anotação CPF/CNPJ

--- src/main/java/com/daniel/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/daniel/cursomc/dto/ClienteNewDTO.java
@@ -2,28 +2,46 @@ package com.daniel.cursomc.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
-import com.daniel.cursomc.domain.Categoria;
+import com.daniel.cursomc.services.validation.ClienteInsert;
 
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	
 
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty(message="Preencimento obrigatório")
+	@Length(min=5,max=80,message = "O tamanho deve ser entre 5 e 80 caracteres!")
 	private String nome;
+	
+	@Email(message="E-mail Inválido")
+	@NotEmpty(message="Preencimento obrigatório")
 	private String email;
+	
+	@NotEmpty(message="Preencimento obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@NotEmpty(message="Preencimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message="Preencimento obrigatório")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message="Preencimento obrigatório")
 	private String cep;	
 	
+	@NotEmpty(message="Preencimento obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
