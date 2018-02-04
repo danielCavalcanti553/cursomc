4a4df817b320676b83d2c1d1374e128668232f47
Daniel Cavalcanti <daniel.cavalcanti@outlook.com.br>
2018-02-04 03:03:11 -0200

    Valor Total e Sub Total de Pedidos

--- src/main/java/com/daniel/cursomc/domain/ItemPedido.java
+++ src/main/java/com/daniel/cursomc/domain/ItemPedido.java
@@ -31,6 +31,11 @@ public class ItemPedido implements Serializable{
 		this.preco = preco;
 	}
 	
+	// GET para ser reconhecido no JSON
+	public double getSubTotal() {
+		return (preco-desconto) * quantidade;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
