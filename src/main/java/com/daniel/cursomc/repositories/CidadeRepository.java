89d92168437b4787fbaedea3982307c0040492e7
daniel.cavalcanti@outlook.com.br <daniel.cavalcanti@outlook.com.br>
2018-02-17 02:03:08 -0200

    End Point Busca Clientes e Estados

--- src/main/java/com/daniel/cursomc/repositories/CidadeRepository.java
+++ src/main/java/com/daniel/cursomc/repositories/CidadeRepository.java
@@ -1,11 +1,18 @@
 package com.daniel.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.daniel.cursomc.domain.Cidade;
 
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+	@Query("SELECT obj FROM Cidade obj WHERE obj.estado.id = :estadoId ORDER BY obj.nome")
+	public List<Cidade> findCidade(@Param("estadoId") Integer estado_id);
 	
 }
